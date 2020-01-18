5a3f6945a1f5137e942f2104049506d84d08fa11
SoHye Choi <wergreat10@naver.com>
2020-01-19 00:12:35 +0900

    Add getBlogPage function

---
+++ src/lib/getBlogPage.js
@@ -0,0 +1,30 @@
+const fetch = require('node-fetch');
+const { BLOG_UUID, API_ENDPOINT, NOTION_TOKEN } = require('./manifest');
+
+async function getBlogPage() {
+    let posts = [];
+    const res = await fetch(`${API_ENDPOINT}`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          cookie: `token_v2=${NOTION_TOKEN}`,
+        },
+        body: JSON.stringify({
+            pageId: BLOG_UUID,
+            limit: 4,
+            cursor: { stack: [] },
+            chunkNumber: 0,
+            verticalColumns: false,
+        }),
+    });
+
+    if(res.ok) {
+        return res.json().then(json => json.recordMap.block[BLOG_UUID].value.content);
+    }
+}
+
+getBlogPage().then(res => {
+    console.log(res);
+});
+
+module.exports = {getBlogPage};
