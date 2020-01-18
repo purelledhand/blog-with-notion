c97da64dc7cc027dd4216b789301a9322cc26ee9
SoHye Choi <wergreat10@naver.com>
2020-01-18 23:01:52 +0900

    Add menifest, config

---
+++ src/lib/manifest.js
@@ -0,0 +1,28 @@
+const { BLOG_ID, TOKEN } = require('./config.js');
+
+const path = require('path');
+
+const normalizeUuid = id => {
+  if (!id) {
+    throw new Error(
+        `BLOG_ID in config.js is empty.`
+    )
+  }
+  if (id.length === 36 && id[8] === '-' && id[12] === '-' && id[16] === '-') return id;
+  return `${id.substr(0, 8)}-${id.substr(8, 4)}-${id.substr(12, 4)}-${id.substr(
+    16,
+    4
+  )}-${id.substr(20)}`
+}
+
+console.log(normalizeUuid(BLOG_ID));
+
+const NOTION_TOKEN = TOKEN;
+const BLOG_UUID = normalizeUuid(BLOG_ID);
+const API_ENDPOINT = 'https://www.notion.so/api/v3/loadPageChunk';
+
+module.exports = {
+  NOTION_TOKEN,
+  BLOG_UUID,
+  API_ENDPOINT,
+}
