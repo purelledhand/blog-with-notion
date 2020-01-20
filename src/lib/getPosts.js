215238e801e9392685a73dae5618be357847945d
SoHye Choi <wergreat10@naver.com>
2020-01-20 21:06:54 +0900

    return response promise

--- src/lib/getPosts.js
+++ src/lib/getPosts.js
@@ -6,9 +6,7 @@ const { API_ENDPOINT, NOTION_TOKEN } = require('./manifest');
 async function getPostContents() {
     postIds = await getBlogPage();
 
-    console.log(postIds);
-
-    const postContent = await postIds.map(async post => {
+    const postContent = postIds.map(async post => {
         const res = await fetch(`${API_ENDPOINT}`, {
             method: 'POST',
             headers: {
@@ -26,20 +24,30 @@ async function getPostContents() {
 
         if(res.ok) {
             let postContents = await res.json();
+            
             const contentIds = postContents.recordMap.block[post].value.content;
-            postContents = contentIds.map( id => {
-                const body = postContents.recordMap.block[id].value;
-                if(body.properties.title[0]) {
-                    return {
-                        "type": body.type,
-                        "title": body.properties.title[0]
-                    };
-                }
-                
-            });
-            console.log(postContents);
+            
+            try {
+                return contentIds.map(id => {
+                    const body = postContents.recordMap.block[id].value;
+                    if('properties' in body && 'title' in body.properties) {
+                        return {
+                            "type": body.type,
+                            "title": body.properties.title[0][0]
+                        };
+                    }
+
+                    return null;
+                });
+            } catch (e) {
+                return null;
+            }
         }
     });
+
+    return await Promise.all(postContent);
 }
 
-getPostContents();
+getPostContents()
+    .then(r => console.log(r))
+    .catch(e => console.log('C'));
